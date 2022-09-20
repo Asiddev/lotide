b550dd7ae13f7b3f2d0c51c68bd1e9743b947f94
Alexander Sidorenko <asiddev@protonmail.com>
2022-09-19 18:11:38 -0700

    assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed:${arr1} === ${arr2} `);
+  } else console.log(`🛑🛑🛑 Assertion Failed:${arr1} !== ${arr2} `);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
