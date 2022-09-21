6b4cba093accb0f910712d0a5d047fc4108d3a38
Alexander Sidorenko <asiddev@protonmail.com>
2022-09-21 10:28:01 -0700

    implemented findKey

---
+++ findKey.js
@@ -0,0 +1,62 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:${actual} === ${expected} `);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected} `);
+  }
+};
+
+const findKey = function (obj, cb) {
+  let result = [];
+  for (let key of Object.keys(obj)) {
+    if (cb(obj[key])) {
+      result.push(key);
+      console.log(typeof key);
+      break;
+    }
+  }
+
+  return result.join("");
+};
+
+// findKey(
+//   {
+//     "Blue Hill": { stars: 1 },
+//     Akaleri: { stars: 3 },
+//     noma: { stars: 2 },
+//     elBulli: { stars: 3 },
+//     Ora: { stars: 2 },
+//     Akelarre: { stars: 3 },
+//   },
+//   (x) => x.stars === 2
+// ); // => "noma"
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
+);
+// assertEqual(
+//   findKey(
+//     {
+//       "Blue Hill": { stars: 1 },
+//       Akaleri: { stars: 3 },
+//       noma: { stars: 2 },
+//       elBulli: { stars: 3 },
+//       Ora: { stars: 2 },
+//       Akelarre: { stars: 3 },
+//     },
+//     (x) => x.stars === 3
+//   ),
+//   "Akaleri"
+// );
