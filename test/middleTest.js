d8065f1b5e5a154fde0f069cc49fdf4aaac854ab
Alexander Sidorenko <asiddev@protonmail.com>
2022-09-25 22:54:50 -0700

    using deepEqual instead

--- test/middleTest.js
+++ test/middleTest.js
@@ -4,13 +4,9 @@
 
 const assert = require("chai").assert;
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
-const eqArrays = require("../eqArrays");
 
 describe("#middleTest", () => {
   it("returns true for [1, 2, 3] [2]", () => {
-    assert.strictEqual(
-      assertArraysEqual(eqArrays(middle([1, 2, 3]), [2]), true)
-    );
+    assert.deepEqual(middle([1, 2, 3]), [2]);
   });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -6,11 +6,13 @@
 // assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
 
 const assert = require("chai").assert;
-const eqArrays = require("../eqArrays");
 const tail = require("../tail");
 
 describe("#tailTest", () => {
-  it("returns true for [5, 6, 7] [6, 7]", () => {
-    assert.strictEqual(eqArrays(tail([5, 6, 7]), [6, 7]), true);
+  it("should return [LightHouse, Labs] for [Yo Yo ,Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), [
+      "Lighthouse",
+      "Labs",
+    ]);
   });
 });
