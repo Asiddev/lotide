eddcd8fc023014ac6528f3c23009cfe79bfa4f95
Alexander Sidorenko <asiddev@protonmail.com>
2022-10-11 21:40:56 -0700

    testing commit

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,7 @@
 // console.log(middle([1, 2, 3, 4, 5, 6, 7, 7, 6]));
 // console.log(middle([1, 2, 3, 4, 5, 6, 7]));
 // console.log();
+console.log("hi");
 
 const assert = require("chai").assert;
 const middle = require("../middle");
